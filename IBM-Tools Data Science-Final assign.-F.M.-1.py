9bbd0c22990a4bb420d98c174475492d5055cb0d
fer-github2023 <135663555+fer-github2023@users.noreply.github.com>
2023-06-11 17:57:34 +0100

    Add files via upload

---
+++ IBM-Tools Data Science-Final assign.-F.M.-1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# **Objectives**
+# * Module 1: Overview of Data Science tools
+# * Module 2: Languages of Data Science (List popular languages for Data Science)
+# * Module 3: Libraries, APIs, Datasets, and Models
+# * Module 4: Jupyter Project
+# * Module 5: Rstudio and Github
+
+# # In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ## Some of the popular languages that Data Scientists use are:
+# 1. R
+# 2. Julia
+# 3. Python
+# 4. SQL
+
+# ## Some of the commonly used libraries used by Data Scientists include:
+# 1. NumPy
+# 2. SciPy
+# 3. Pandas
+# 4. Keras
+# 5. SciKit-Learn
+# 6. PyTorch
+
+# | Data Science Tools |
+# | ------ | 
+# | RStudio |
+# | Apache Spark |
+# | TensorFlow |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# ### This a simple arithmetic expression to mutiply then add integers 
+
+# In[3]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# Fernando Mourino
+
+# In[ ]:
+
+
+
+
